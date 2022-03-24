d52daf5df855f6758f7e3ed46d05a8d57cbdcb4f
Mir__off__Shoh <98812035+pubgmobileplay@users.noreply.github.com>
2022-03-24 17:24:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+let x = prompt("o'z ismingizni kiriting")
+
+let y = prompt("yoshingizni kiriting")
+
+
+
+var name = "  x"
+
+console.log( 'Mening Ismim '  + x);
+
+var years = " y"
+
+console.log( 'Men ' + y +" yoshdaman");
+
+let misol = prompt(" Misol 1:    10 + 10=")
+
+console.log('Misol 1: 10 + 10 = 20 sizning javobingiz ' + misol);
+
+let misol2 = prompt(" Misol 2:    20 - 5=")
+
+console.log('Misol 2: 20 - 5 = 15 sizning javobingiz ' + misol2);
+
+let misol1 = prompt(" Misol 3:    15 * 2=")
+
+console.log('Misol 3: 15 * 2 = 30 sizning javobingiz ' + misol1);
+
+let misol3 = prompt(" Misol 4:    8 / 2=")
+
+console.log('Misol 4: 8 / 4 = 2 sizning javobingiz ' + misol3);
+
+let misol4 = prompt(" Misol 4:    10 % 3=")
+
+console.log('Misol 5: 10 % 3 = 1 sizning javobingiz ' + misol4);
+
+alert("Natijani ko`rish uchun konsolni oching") 
